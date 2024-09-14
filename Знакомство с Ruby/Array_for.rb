17553017c78b2ef7a7895754735c42e548b223f8
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-09-14 14:49:54 +0300

    Snake case update

--- Знакомство с Ruby/Array_for.rb
+++ Знакомство с Ruby/Array_for.rb
@@ -1,6 +1,6 @@
 array = [10, 2, 3, 4, 5]
 
-def Mininal_element arr
+def minimal_element arr
 	minim=arr[0]
 	for i in(0..arr.size-1)
 		if (arr[i] < minim)
@@ -9,20 +9,20 @@ def Mininal_element arr
 	end
 	return minim
 end
-print "Вывожу минимальный элемент массива: #{Mininal_element(array)}\n"
+print "Вывожу минимальный элемент массива: #{minimal_element(array)}\n"
 
 
-def Element_print arr
+def elements_print arr
 	print "Вывожу элементы массива:"
 	for i in(0..arr.size-1)
 		print "#{arr[i]}  "
 	end
 	print "\n"
 end
-Element_print(array)
+elements_print(array)
 
 
-def First_positive_elem arr
+def first_positive_elem arr
 	for i in(0..arr.size-1)
 		if (arr[i] > 0)
 			return arr[i]
@@ -30,4 +30,4 @@ def First_positive_elem arr
 		end
 	end
 end
-print "Вывожу первый положительный элемент массива: #{First_positive_elem(array)}"
+print "Вывожу первый положительный элемент массива: #{first_positive_elem(array)}"
