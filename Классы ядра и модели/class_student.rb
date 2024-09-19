edac3382677af8a9326a8728f3634d44065bcc5a
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-09-20 01:49:10 +0300

    Avoid repetitions in constructor, getter, setter

--- Классы ядра и модели/class_student.rb
+++ Классы ядра и модели/class_student.rb
@@ -1,46 +1,6 @@
 class Student
-	def id
-		@id
-	end
-	def id=(id)
-		@id = id
-	end
-	def first_name
-		@first_name
-	end
-	def first_name=(first_name)
-		@first_name = first_name
-	end
-	def second_name
-		@second_name
-	end
-	def second_name=(second_name)
-		@second_name = second_name
-	end
-	def third_name
-		@third_name
-	end
-	def third_name=(third_name)
-		@third_name = third_name
-	end
-	def telephone
-		@telephone
-	end
-	def telephone=(telephone)
-		@telephone=telephone
-	end
-	def telegram
-		@telegram
-	end
-	def telegram=(telegram)
-		@telegram=telegram
-	end
-	def git
-		@git
-	end
-	def git=(git)
-		@git=git
-	end
+	attr_reader :id, :first_name, :second_name, :third_name, :telephone, :telegram, :git
+	attr_writer :id, :first_name, :second_name, :third_name, :telephone, :telegram, :git
 	def initialize(first_name,second_name,third_name,id = nil,telephone = nil,telegram = nil,git = nil)
 		self.id = id
 		self.first_name = first_name
