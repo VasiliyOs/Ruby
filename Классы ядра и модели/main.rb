cce307bf714c5a42525ab82ce9118cde1c3cdc17
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-09-20 01:23:44 +0300

    Add file with create and print class data

---
+++ Классы ядра и модели/main.rb
@@ -0,0 +1,5 @@
+require 'C:\Users\Василий Осипов\class_student.rb'
+st1 = Student.new("Имя1","Фамилия1","Отчёство1",1,nil,nil,"Гит1")
+st2 = Student.new("Имя2","Фамилия2","Отчёство2",2,"8(800)555 35 35",nil, nil)
+st1.print_student
+st2.print_student
