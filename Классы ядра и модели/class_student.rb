8b647e12baf61d846e992b4c09eb4667c21c4db8
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-09-27 23:16:16 +0300

    LR2.N1.1 "Creating class Student"

--- Классы ядра и модели/class_student.rb
+++ Классы ядра и модели/class_student.rb
@@ -1,5 +1,4 @@
 class Student
-	attr_accessor :id, :first_name, :second_name, :third_name, :telephone, :telegram, :git
 	def id
 		@id
 	end
@@ -42,7 +41,13 @@ class Student
 	def git=(git)
 		@git=git
 	end
-	def to_s
-		"Имя студента:#{self.first_name}\n Фамилия студента:#{self.second_name}\n Отчество студента:#{self.third_name}\n ID студента:#{self.id}\n Телефон студента:#{self.telephone}\n Телеграм студента:#{self.telegram}\n Гит студента:#{self.git}\n"
+	def initialize(first_name,second_name,third_name,id = nil,telephone = nil,telegram = nil,git = nil)
+		self.id = id
+		self.first_name = first_name
+		self.second_name = second_name
+		self.third_name = third_name
+		self.telephone = telephone
+		self.telegram = telegram
+		self.git = git
 	end
 end
