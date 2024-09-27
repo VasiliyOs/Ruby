32a8c68a472e1c12b218e5f31b0d02369ebe32fc
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-09-27 23:10:50 +0300

    LR2.N1.2 "Add method to_s"

--- Классы ядра и модели/class_student.rb
+++ Классы ядра и модели/class_student.rb
@@ -42,13 +42,7 @@ class Student
 	def git=(git)
 		@git=git
 	end
-	def initialize(first_name,second_name,third_name,id = nil,telephone = nil,telegram = nil,git = nil)
-		self.id = id
-		self.first_name = first_name
-		self.second_name = second_name
-		self.third_name = third_name
-		self.telephone = telephone
-		self.telegram = telegram
-		self.git = git
+	def to_s
+		"Имя студента:#{self.first_name}\n Фамилия студента:#{self.second_name}\n Отчество студента:#{self.third_name}\n ID студента:#{self.id}\n Телефон студента:#{self.telephone}\n Телеграм студента:#{self.telegram}\n Гит студента:#{self.git}\n"
 	end
 end
