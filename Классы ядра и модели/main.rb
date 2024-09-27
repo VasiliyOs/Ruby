7722d42e0c4c4655105ca80953f14871183999cb
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-09-27 23:09:04 +0300

    LR2.N1.2 "Enter, print class data

--- Классы ядра и модели/main.rb
+++ Классы ядра и модели/main.rb
@@ -1,5 +1,5 @@
 require 'C:\Users\Василий Осипов\class_student.rb'
-st1 = Student.new("Имя1", "Отчество1", "Фмилия1", git: nil, telephone: "8(800)555 35 35", telegram: nil, id: 1)
-st2 = Student.new("Имя2", "Отчество2", "Фмилия2", id: 2, telephone: "8(800)555 35 35", git: nil, telegram: nil)
-st1.print_student
-st2.print_student
+st1 = Student.new("Имя1","Фамилия1","Отчёство1",1,nil,nil,"Гит1")
+st2 = Student.new("Имя2","Фамилия2","Отчёство2",2,"8(800)555 35 35",nil, nil)
+puts st1.to_s
+puts st2.to_s
