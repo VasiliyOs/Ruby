f8194ee35169d0b018e2b0c4b8ef3993deecaa86
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-09-27 23:23:44 +0300

    LR2.N1.3 "Add attr_reader, attr_writer"

--- Классы ядра и модели/class_student.rb
+++ Классы ядра и модели/class_student.rb
@@ -1,46 +1,6 @@
 class Student
-	def id
-		@id
-	end
-	def id=(id)
-		@id = id
-	end
-	def first_name
-		@first_name
-	end
-	def first_name=(first_name)
-		@first_name = first_name
-	end
-	def second_name
-		@second_name
-	end
-	def second_name=(second_name)
-		@second_name = second_name
-	end
-	def third_name
-		@third_name
-	end
-	def third_name=(third_name)
-		@third_name = third_name
-	end
-	def telephone
-		@telephone
-	end
-	def telephone=(telephone)
-		@telephone=telephone
-	end
-	def telegram
-		@telegram
-	end
-	def telegram=(telegram)
-		@telegram=telegram
-	end
-	def git
-		@git
-	end
-	def git=(git)
-		@git=git
-	end
+	attr_reader :id, :first_name, :second_name, :third_name, :telephone, :telegram, :git
+	attr_writer :id, :first_name, :second_name, :third_name, :telephone, :telegram, :git
 	def initialize(first_name,second_name,third_name,id = nil,telephone = nil,telegram = nil,git = nil)
 		self.id = id
 		self.first_name = first_name
@@ -52,4 +12,5 @@ class Student
 	end
 	def to_s
 		"Имя студента:#{self.first_name}\n Фамилия студента:#{self.second_name}\n Отчество студента:#{self.third_name}\n ID студента:#{self.id}\n Телефон студента:#{self.telephone}\n Телеграм студента:#{self.telegram}\n Гит студента:#{self.git}\n"
+	end
 end
