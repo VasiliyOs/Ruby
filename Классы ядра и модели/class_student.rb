b066def80ef832b2b19aa9baea52085a5ac2d78f
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-09-27 23:38:52 +0300

    LR2.N1.6 "Hash constructor"

--- Классы ядра и модели/class_student.rb
+++ Классы ядра и модели/class_student.rb
@@ -1,13 +1,13 @@
 class Student
 	attr_accessor :id, :first_name, :second_name, :third_name, :telephone, :telegram, :git
-	def initialize(first_name, second_name, third_name, id: nil, telephone: nil, telegram: nil, git: nil)
-		self.id = id
+	def initialize(first_name, second_name, third_name, info = {})
+		self.id = info[:id] 
 		self.first_name = first_name
 		self.second_name = second_name
 		self.third_name = third_name
-		self.telephone = telephone
-		self.telegram = telegram
-		self.git = git
+		self.telephone = info[:telephone]
+		self.telegram = info[:telegram]
+		self.git = info[:git]
 	end
 	def to_s
 		"Имя студента:#{self.first_name}\n Фамилия студента:#{self.second_name}\n Отчество студента:#{self.third_name}\n ID студента:#{self.id}\n Телефон студента:#{self.telephone}\n Телеграм студента:#{self.telegram}\n Гит студента:#{self.git}\n"
