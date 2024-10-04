390d1814814a46ee3e384e334d16a19302500abc
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-10-05 00:50:14 +0300

    LR2.N2.2 "Test new parsing constructor"

--- Классы ядра и модели/main.rb
+++ Классы ядра и модели/main.rb
@@ -1,5 +1,5 @@
 require 'C:\Users\Василий Осипов\class_student.rb'
-st1 = Student.new("Имя1", "Отчество1", "Фмилия1", git: nil, telephone: "8(800)555 35 35", telegram: nil, id: 1)
-st2 = Student.new("Имя2", "Отчество2", "Фмилия2", id: 2, telephone: "8(800)555 35 35", git: nil, telegram: nil)
-puts st1.to_s
-puts st2.to_s
+
+str = "first_name: Имя;second_name: Фамилия;third_name: Отчество;id: 2;Telephone:; Email: abc@mail.ru; git:; telegram:"
+st3 = Student.create_from_pars_string(str)
+print st3.to_s
