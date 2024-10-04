7aa3093910bb38750f9101f25281ac984b703cfa
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-10-05 00:42:34 +0300

    LR2.N2.4 "Test Student_short"

--- Классы ядра и модели/super_main.rb
+++ Классы ядра и модели/super_main.rb
@@ -1,4 +1,11 @@
 require './super_class_student.rb'
-str = "first_name: Имя;second_name: Фамилия;third_name: Отчество;id: 2;Telephone:; Email: abc@mail.ru; git:; telegram:"
+require './class_short_student.rb'
+str = "first_name: Имя;second_name: Фамилия;third_name: Отчество;id: 2;Telephone:8 800 555 35 35; Email: abc@mail.ru; git:arab; telegram:@Jack12"
 st1 = Student.create_from_pars_string(str)
-print st1.to_s
+puts st1.get_info
+sh_str = "id: 3; second_name_and_initials: Осипов В.Р.; git: aboba; contact: abc@mail.ru"
+sh_st1 = Student_short.create_from_pars_string(sh_str)
+puts sh_st1.get_info
+
+sh_st2 = Student_short.new(id: 3, second_name_and_initials: "Осипов В.Р.", git: "aboba", contact:"@Jack228")
+puts sh_st2.to_s
