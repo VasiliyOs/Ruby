82a9aae9f078513e376795b5351989c46b9cb955
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-10-12 01:24:57 +0300

    LR2.N1.10 "Add method set_contacts"

--- Классы ядра и модели/Student.rb
+++ Классы ядра и модели/Student.rb
@@ -121,20 +121,21 @@ class Student
     end
   end
 
+  def set_contact(telephone, telegram, email)
+    self.telephone = telephone
+    self.telegram = telegram
+    self.email = email
+  end
 
   def initialize(first_name,second_name,third_name,id: nil,telephone: nil,telegram: nil,email: nil,git: nil)
     self.id = id
     self.first_name = first_name
     self.second_name = second_name
     self.third_name = third_name
-    self.telephone = telephone
-    self.telegram = telegram
-    self.email = email
     self.git = git
+    set_contact(telephone, telegram, email)
   end
   def to_s
     "Имя:#{self.first_name}\nФамилия:#{self.second_name}\nОтчество:#{self.third_name}\nID:#{self.id}\nТелефон:#{self.telephone}\nТелегрма:#{self.telegram}\nEmail:#{self.email}\nGit:#{self.git}\n\n\n"
   end
 end
-
-
