0a260a6a70c137164307615447c80f90f75bc72f
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-10-12 00:53:49 +0300

    LR2.N1.3 "Add attr_reader, attr_writer"

--- Классы ядра и модели/Student.rb
+++ Классы ядра и модели/Student.rb
@@ -1,52 +1,8 @@
 class Student
-  def id
-    @id
-  end
-  def id=(id)
-    @id = id
-  end
-  def first_name
-    @first_name
-  end
-  def first_name=(first_name)
-    @first_name = first_name
-  end
-  def second_name
-    @second_name
-  end
-  def second_name=(second_name)
-    @second_name = second_name
-  end
-  def third_name
-    @third_name
-  end
-  def third_name=(third_name)
-    @third_name = third_name
-  end
-  def telephone
-    @telephone
-  end
-  def telephone=(telephone)
-    @telephone=telephone
-  end
-  def telegram
-    @telegram
-  end
-  def telegram=(telegram)
-    @telegram=telegram
-  end
-  def git
-    @git
-  end
-  def git=(git)
-    @git=git
-  end
-  def email
-    @email
-  end
-  def email=(email)
-    @email = email
-  end
+  
+  attr_reader :id, :first_name, :second_name, :third_name, :telephone, :telegram, :email, :git
+  attr_writer :id, :first_name, :second_name, :third_name, :telephone, :telegram, :email, :git
+  
   def initialize(first_name,second_name,third_name,id = nil,telephone = nil,telegram = nil,email = nil,git = nil)
     self.id = id
     self.first_name = first_name
@@ -61,4 +17,3 @@ class Student
     "Имя:#{self.first_name}\nФамилия:#{self.second_name}\nОтчество:#{self.third_name}\nID:#{self.id}\nТелефон:#{self.telephone}\nТелегрма:#{self.telegram}\nEmail:#{self.email}\nGit:#{self.git}\n\n\n"
   end
 end
-
