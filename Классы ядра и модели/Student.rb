525b1a368d93411abd97b29b01808dbe256649d1
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-10-12 00:55:10 +0300

    LR2.N1.5 "Any order optional fields"

--- Классы ядра и модели/Student.rb
+++ Классы ядра и модели/Student.rb
@@ -2,7 +2,7 @@ class Student
 
   attr_accessor :id, :first_name, :second_name, :third_name, :telephone, :telegram, :email, :git
 
-  def initialize(first_name,second_name,third_name,id = nil,telephone = nil,telegram = nil,email = nil,git = nil)
+  def initialize(first_name,second_name,third_name,id: nil,telephone: nil,telegram: nil,email: nil,git: nil)
     self.id = id
     self.first_name = first_name
     self.second_name = second_name
@@ -16,3 +16,4 @@ class Student
     "Имя:#{self.first_name}\nФамилия:#{self.second_name}\nОтчество:#{self.third_name}\nID:#{self.id}\nТелефон:#{self.telephone}\nТелегрма:#{self.telegram}\nEmail:#{self.email}\nGit:#{self.git}\n\n\n"
   end
 end
+
