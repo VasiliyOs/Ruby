7b5c6fdd9eba27b2dd1ea80d08632943fce457b1
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-10-12 00:55:45 +0300

    LR2.N1.5 "Enter data any order"

--- Классы ядра и модели/main.rb
+++ Классы ядра и модели/main.rb
@@ -1,4 +1,4 @@
 require 'C:\Users\maise\RubymineProjects\untitled\Student.rb'
 
-st1 = Student.new("Имя", "Фамилия","Отчество", 2, "8 800 555 35 35", "@Jack123", "abc@mail.ru", "VasiliyOs")
+st1 = Student.new("Имя", "Фамилия","Отчество", id: 2, telephone: "8 800 555 35 35", telegram: "@Jack123", git: "VasiliyOs", email: "abc@mail.ru")
 puts st1.to_s
