0db72da6958b384db35198e18b480caa8afb6135
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-10-12 00:53:02 +0300

    LR2.N1.2 "Print Student data"

---
+++ Классы ядра и модели/main.rb
@@ -0,0 +1,4 @@
+require 'C:\Users\maise\RubymineProjects\untitled\Student.rb'
+
+st1 = Student.new("Имя", "Фамилия","Отчество", 2, "8 800 555 35 35", "@Jack123", "abc@mail.ru", "VasiliyOs")
+puts st1.to_s
