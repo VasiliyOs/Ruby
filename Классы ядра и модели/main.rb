9bda79f5443e2052614e476133cb2af1c82ad124
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-10-12 02:00:02 +0300

    LR2.N2.3 "Test get_info"

--- Классы ядра и модели/main.rb
+++ Классы ядра и модели/main.rb
@@ -1,4 +1,7 @@
 require 'C:\Users\maise\RubymineProjects\untitled\Student.rb'
 
 st1 = Student.new("Имя", "Фамилия","Отчество", id: 2, telephone: "8 800 555 35 35", telegram: "@Jack123", git: "VasiliyOs", email: "abc@mail.ru")
-puts st1.to_s
+putst st1.to_s
+st2 = Student.pars("first_name: Имя;second_name: Фамилия;third_name: Отчество;id: 3;Telephone:8 999 999 99 99; Email: abc@mail.ru; git:VasiliyOs; telegram:@Jackobs22")
+print st2.to_s
+puts st1.get_info
