fcd4d48f03b957d2ec9d5bab1d5f94a30a4b9ebc
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-10-12 02:00:48 +0300

    LR2.N2.4 "Test Student_short"

--- Классы ядра и модели/main.rb
+++ Классы ядра и модели/main.rb
@@ -1,7 +1,12 @@
+require 'C:\Users\maise\RubymineProjects\untitled\Student_short.rb'
 require 'C:\Users\maise\RubymineProjects\untitled\Student.rb'
 
 st1 = Student.new("Имя", "Фамилия","Отчество", id: 2, telephone: "8 800 555 35 35", telegram: "@Jack123", git: "VasiliyOs", email: "abc@mail.ru")
-putst st1.to_s
 st2 = Student.pars("first_name: Имя;second_name: Фамилия;third_name: Отчество;id: 3;Telephone:8 999 999 99 99; Email: abc@mail.ru; git:VasiliyOs; telegram:@Jackobs22")
-print st2.to_s
-puts st1.get_info
+
+st1_s = Student_short.initialize_from_string(id:3, string:"first_name: Имя;second_name: Фамилия;third_name: Отчество;Telephone:8 999 999 99 99; Email: abc@mail.ru; git:VasiliyOs; telegram:@Jackobs22" )
+puts st1_s.to_s
+st2_s = Student_short.student_in_short_student(st2)
+puts st2_s.to_s
+st2_s = Student_short.student_in_short_student(st1)
+puts st2_s.to_s
