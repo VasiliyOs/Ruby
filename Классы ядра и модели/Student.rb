424ea6c5a06bc91e99c80c4db4e6034d4a5149e6
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-10-12 00:48:36 +0300

    LR2.N1.1 "Creating class Student"

---
+++ Классы ядра и модели/Student.rb
@@ -0,0 +1,54 @@
+class Student
+  def id
+    @id
+  end
+  def id=(id)
+    @id = id
+  end
+  def first_name
+    @first_name
+  end
+  def first_name=(first_name)
+    @first_name = first_name
+  end
+  def second_name
+    @second_name
+  end
+  def second_name=(second_name)
+    @second_name = second_name
+  end
+  def third_name
+    @third_name
+  end
+  def third_name=(third_name)
+    @third_name = third_name
+  end
+  def telephone
+    @telephone
+  end
+  def telephone=(telephone)
+    @telephone=telephone
+  end
+  def telegram
+    @telegram
+  end
+  def telegram=(telegram)
+    @telegram=telegram
+  end
+  def git
+    @git
+  end
+  def git=(git)
+    @git=git
+  end
+  def initialize(first_name,second_name,third_name,id = nil,telephone = nil,telegram = nil,email = nil,git = nil)
+    self.id = id
+    self.first_name = first_name
+    self.second_name = second_name
+    self.third_name = third_name
+    self.telephone = telephone
+    self.telegram = telegram
+    self.git = git
+  end
+end
+
