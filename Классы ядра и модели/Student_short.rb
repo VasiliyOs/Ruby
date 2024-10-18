aaf023950bacda56ee0a2c671d107b7b73fb194e
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-10-19 00:38:13 +0300

    LR2.N2.5 "Student_short inherited from People"

--- Классы ядра и модели/Student_short.rb
+++ Классы ядра и модели/Student_short.rb
@@ -1,14 +1,26 @@
 require 'C:\Users\maise\RubymineProjects\untitled\People.rb'
 class Student_short < People
-  attr_accessor :full_name, :contact
 
-  def initialize(first_name, second_name, third_name, id: nil, git: nil, contact: nil)
+  attr_reader :full_name, :contact_name
+
+  def initialize(first_name, second_name, third_name, id: nil, git: nil, contact: nil, contact_name: nil)
     super(first_name, second_name, third_name, git: git, id: id)
-    self.full_name = "#{second_name} #{first_name[0]}. #{third_name[0]}."
+    self.full_name = full_name
     self.contact = contact
+    self.contact_name = contact_name
   end
 
   private_class_method :initialize
+
+  def full_name
+    super
+  end
+  def full_name=(full_name)
+    @full_name = full_name
+  end
+  def contact_name=(contact_name)
+    @contact_name = contact_name
+  end
 
   def Student_short.pars(str)
     super(str)
@@ -27,7 +39,7 @@ class Student_short < People
     return super.git, "#{get_contact(telephone, telegram, email)[0]}", "#{get_contact(telephone,telegram, email)[1]}", super.second_name, super.first_name, super.third_name
   end
 
-  def Student_short.get_contact(telephone, telegram, email)
+  def Student_short.get_contact(telephone, telegram, email) #Метод класса так как я его должен использовать без экземпляра, он применяется в методе класса pars для парса строки и нахождения контакта. Вроде можно добавть телефон и другие контакты и использовать super. Ещё можно дополнить метод pars и находить контакт подругому.
     if telephone != nil
       contact_name = "Телефон"
       contact = telephone
@@ -44,21 +56,23 @@ class Student_short < People
   def Student_short.student_in_short_student(student)
     id = student.id
     git = student.git
-    contact = "#{student.get_contact[0]}:#{student.get_contact[1]}"
-    return Student_short.new(student.first_name,student.second_name,student.third_name,id: id, git: git, contact: contact)
+    contact = student.get_contact[1]
+    contact_name = student.get_contact[0]
+    return Student_short.new(student.first_name,student.second_name,student.third_name,id: id, git: git, contact: contact, contact_name: contact_name)
   end
 
   def Student_short.initialize_from_string(id: nil, string: nil)
     id = id
     git = Student_short.pars(string)[0]
-    contact = "#{Student_short.pars(string)[1]}:#{Student_short.pars(string)[2]}"
+    contact_name = Student_short.pars(string)[1]
+    contact = Student_short.pars(string)[2]
     second_name = Student_short.pars(string)[3]
     first_name = Student_short.pars(string)[4]
     third_name = Student_short.pars(string)[5]
-    return Student_short.new(first_name, second_name, third_name,id: id, git: git, contact: contact)
+    return Student_short.new(first_name, second_name, third_name,id: id, git: git, contact: contact, contact_name: contact_name)
   end
 
   def to_s
-    "ФИО:#{full_name}\nID:#{self.id}\nGit:#{self.git}\n#{self.contact}\n\n\n"
+    "ФИО:#{full_name}\nID:#{self.id}\nGit:#{self.git}\n#{self.contact_name}:#{self.contact}\n\n\n"
   end
 end
