f0b193c0eb7cb22648477b26223d6f3e5d008c50
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-11-08 23:01:06 +0300

    LR3.N2.1 "Add method find_all"

--- Blocks. Collections. Trees/Array_handler.rb
+++ Blocks. Collections. Trees/Array_handler.rb
@@ -4,5 +4,15 @@ class Array_handler
   def initialize(array)
     self.array = array
   end
+
+  def find_all
+    new_arr = []
+    array.each do |elem|
+      if yield(elem)
+        new_arr.push(elem)
+      end
+    end
+    new_arr
+  end
 end
 
