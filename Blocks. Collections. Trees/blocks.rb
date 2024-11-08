c68af9dedd5287dc321b0cfb34b9e60b495dd550
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-11-08 20:42:19 +0300

    LR3.N1.5 "Add input and output"

--- Blocks. Collections. Trees/blocks.rb
+++ Blocks. Collections. Trees/blocks.rb
@@ -42,3 +42,58 @@ end
   end
   new_arr
 end
+
+ef input_arr
+  puts "Введите целочисленный массив:"
+  array = gets.chomp.split.map(&:to_i)
+end
+
+
+def from_file_arr(file_path)
+  array = []
+  if file_path.nil? || file_path.empty? || not(File.exist?(file_path))
+    puts 'Путь к файлу не должен быть пустым или файла не существует!'
+  else
+    File.open(file_path, 'r') do |file|
+      array = file.read.split.map(&:to_i)
+    end
+  end
+  array
+end
+
+
+puts "Введите задание от 1 до 5, для отмены введите 0:"
+choice_task = gets.to_i
+puts "Введите тип воода (1-с клавиатуры, 2-из файла):"
+choice_input = gets.to_i
+if choice_input == 1
+  arr = input_arr
+elsif choice_input == 2
+  puts "Введите путь к файлу:"
+  file_path = gets.chomp
+  arr = from_file_arr(file_path)
+end
+
+def do_task(choice_task,arr)
+  if not(arr.empty?)
+    case choice_task
+    when 1
+      p "Массив с элементами наоборот между максимальным и минимальным:#{backwards_elem(arr){|min_index,max_index,new_arr| new_arr[min_index+1..max_index-1]=new_arr[min_index+1..max_index-1].reverse}}"
+    when 2
+      p "2 максимальных элемента массива:#{find_2_max_elem(arr){|numbers| numbers.last(2)}}"
+    when 3
+      puts "Максимальный нечётный элемент:#{find_max_odd(arr){|max_odds| max_odds.last(1)}}"
+    when 4
+      p "Индексы элемента, который чаще всего встречается:#{find_most_frequent_elem_indexes(arr){|max_count,count| max_count<count}}"
+    when 5
+      p "Элементы, который делятся на свой номер и встречаются 1 раз:#{find_el_del_on_self_index(arr){|new_arr,num| new_arr.push(num)}}"
+    when 0
+      puts "Выбор задачи отменён!"
+    else
+      puts "Неверный выбор номера задачи!"
+    end
+  else
+    puts "Массив пуст!"
+  end
+end
+do_task(choice_task,arr)
