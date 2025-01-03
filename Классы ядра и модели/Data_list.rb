d3452cb35eb4171152f26826c0e66a840c0e00fd
VasiliyOs <vasiliy2809_ne@mail.ru>
2025-01-03 10:55:11 +0300

    LR4.N1.4 "Add methods select and other"

--- Классы ядра и модели/Data_list.rb
+++ Классы ядра и модели/Data_list.rb
@@ -4,14 +4,37 @@ class Data_list
   private :array, :array=
 
   def initialize(array)
-    self.array = array.sort
+    self.array = []
+    array.sort.each do |elem|
+      self.array << ({data: elem, select: false})
+    end
   end
 
   def [](ind)
-    if !array[ind].nil?
+    if !self.array[ind].nil?
       self.array[ind]
     else
       raise IndexError.new("Неверный индекс")
     end
   end
+
+  def select(ind)
+    if !self.array[ind].nil?
+      self.array[ind][:select] = true
+    end
+  end
+
+  def get_selected
+    selected_arr = self.array.find_all{|elem| elem[:select] == true}
+  end
+
+  def get_names
+
+  end
+  protected :get_names
+
+  def get_data
+
+  end
+  private :get_data
 end
