586f4629532c5de33d6a7086322d04ea9c34febd
VasiliyOs <vasiliy2809_ne@mail.ru>
2025-01-06 01:49:20 +0300

    LR4.N2.6 "Make strategy"

--- Классы ядра и модели/Student_list.rb
+++ Классы ядра и модели/Student_list.rb
@@ -1,23 +1,38 @@
 require './Student.rb'
 require './Student_short.rb'
 require './data_list_student_short.rb'
-
+require 'C:\Users\Василий Осипов\Desktop\rb444\Strategy_lits.rb'
 class Student_list
 
-  attr_accessor :filepath
+  include Comparable
+
+  attr_accessor :filepath, :strategy
   attr_reader :student_array
 
-  def initialize(filepath: , student_array: nil)
+  def initialize(filepath: , strategy:, student_array: nil)
     self.filepath = filepath
     self.student_array = student_array
+    self.strategy = strategy
   end
-
   def student_array=(student_array)
     if student_array.nil? or student_array.is_a?(Array)
       @student_array = student_array
     else
       raise TypeError.new('Неверный тип входных данных')
     end
+  end
+
+  def write_to_file
+    student_array.each do |elem1|
+      if self.student_array.any?{|elem2| elem2 == elem1 and !elem1.equal?(elem2)}
+        raise ArgumentError.new("В массиве присутсвтуют 2 или более элементов с одинаковыми харакетристиками!!!")
+      end
+    end
+    self.strategy.write_to_file(self.filepath, self.student_array)
+  end
+
+  def read_from_file
+    self.student_array = self.strategy.read_from_file(self.filepath)
   end
 
   private :filepath=, :student_array=
@@ -41,10 +56,16 @@ class Student_list
   end
 
   def add_student(student)
+    if self.student_array.any?{|elem| elem == student}
+      raise ArgumentError.new("Элемент с такими характеристиками уже есть!!!")
+    end
     self.student_array.append(student)
   end
 
   def replace_student_by_id(id, new_student)
+    if self.student_array.any?{|elem| elem == new_student}
+      raise ArgumentError.new("Элемент с такими характеристиками уже есть!!!")
+    end
     index = self.student_array.find_index {|student| student.id == id}
     if index
       self.student_array[index] = new_student
